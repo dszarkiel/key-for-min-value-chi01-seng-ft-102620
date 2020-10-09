a4cf885b92ba032b9a51f616d45792b3693c5b2d
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-09 17:25:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   low_key = nil
   low_value = nil
   hash.each do |key, value|
-    if low_value == nil || value < lowest_value
+    if low_value == nil || value < low_value
       low_value = value
       low_key = key
     end
