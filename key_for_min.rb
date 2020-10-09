567ca3e411d91b0eebd5ee8ad337fb5e324ff0b7
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-09 17:14:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   min = 100
+  lowest_key = ""
   name_hash.each do |name, value|
     if value < min
-      min = name
+      min = value
+      name = lowest_key
     end
   end
   puts min
